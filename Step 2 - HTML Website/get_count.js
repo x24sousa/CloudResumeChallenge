bf14ce750c699c3e072f996b21753230e75ff797
Chris Sousa <x24sousa@gmail.com>
2026-05-27 12:53:29 -0600

    changing file structure

--- Step 2 - HTML Website/get_count.js
+++
@@ -1,5 +0,0 @@
-fetch('https://jmic5vdez6.execute-api.us-west-2.amazonaws.com/default/VisitorCountFuntion')
-    .then(response => response.json())
-  	.then(data => {
-  	document.getElementById("VisitorCounter").textContent = data["visitorcount"];
-  })
