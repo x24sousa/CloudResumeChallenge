a739c21a0fe507c8963a5fb7316c08dca23051e4
Chris Sousa <x24sousa@gmail.com>
2026-05-27 11:18:40 -0600

    Finished visitor count

--- Step 2 - HTML Website/get_count.js
+++ Step 2 - HTML Website/get_count.js
@@ -1,7 +1,5 @@
-<script>
 fetch('https://jmic5vdez6.execute-api.us-west-2.amazonaws.com/default/VisitorCountFuntion')
     .then(response => response.json())
   	.then(data => {
   	document.getElementById("VisitorCounter").textContent = data["visitorcount"];
   })
-</script>
